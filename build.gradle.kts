6634ae1dbfa23ff7196ffa28299b188abd4c2c20
Sergey Okatov <sokatov@gmail.com>
2020-07-15 18:01:36 +0500

    s2 Создание Gradle проекта

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "com.otus.first"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
